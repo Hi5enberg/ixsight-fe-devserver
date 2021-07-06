b890e914400c6619c824dbd796eb48e860f7973c
Hi5enberg <morteza.jalilifar@cgm.com>
2021-07-06 13:32:56 +0200

    Redirect SPA routes

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const app = express();
-app.use(express.static('../ixsight-frontend/dist/'))
-app.listen(3000)
+
+app.get(/.*/, (req, res, next) => {
+
+  let spaRoutes = [
+      '/healthdata/',
+      '/benchmarks/'
+  ];
+
+  console.log(req.url)
+  const isSpaRoute = spaRoutes.includes(req.url)
+  if (isSpaRoute) req.url = '/';
+  next();
+});
+
+app.use(express.static('../ixsight-frontend/dist/'));
+app.listen(3000);
