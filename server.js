cefa3f186c9b1299264033216983c40674828569
Hi5enberg <morteza.jalilifar@cgm.com>
2021-07-06 13:34:39 +0200

    Refactor

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const app = express();
 
 app.get(/.*/, (req, res, next) => {
 
-  let spaRoutes = [
-      '/healthdata/',
-      '/benchmarks/'
-  ];
+    // Add SPA routes here to work during development
+    let spaRoutes = [
+        '/healthdata/',
+        '/benchmarks/'
+    ];
 
-  console.log(req.url)
-  const isSpaRoute = spaRoutes.includes(req.url)
-  if (isSpaRoute) req.url = '/';
-  next();
+    console.log(req.url)
+    const isSpaRoute = spaRoutes.includes(req.url)
+    if (isSpaRoute) req.url = '/';
+    next();
 });
 
 app.use(express.static('../ixsight-frontend/dist/'));
